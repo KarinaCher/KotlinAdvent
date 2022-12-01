0e02ad1ad254b96c06c72210c44dc7a523b0dd37
Karina Cherkashina <karina.cherkashina@gmail.com>
2022-12-01 13:20:40 +0100

    Day 1. Part 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var elvesCalories = mutableMapOf<Int, Int>()
+        var index = 1
+        for (line in input) {
+            if (line.isEmpty()) {
+                index = index.plus(1)
+                elvesCalories.put(index.inc(), 0)
+            }
+            else {
+                elvesCalories[index] = elvesCalories[index]?.plus(line.toInt()) ?: 0
+            }
+        }
+        return elvesCalories.maxOf {e -> e.value}
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +20,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 70613)
 
     val input = readInput("Day01")
     println(part1(input))
