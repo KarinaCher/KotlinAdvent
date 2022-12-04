d9435d05af35cf467b87ca18c5ddc2a0e062528a
Karina Cherkashina <karina.cherkashina@gmail.com>
2022-12-04 09:35:52 +0100

    Day 4. Part 2.

--- src/Day04.kt
+++ src/Day04.kt
@@ -24,13 +24,26 @@ fun main() {
     fun part2(input: List<String>): Int {
         var result = 0
 
+        input.forEach {
+            val elves = it.split(",")
+            val firstElves = elves[0].split("-")
+            val secondElves = elves[1].split("-")
+
+            val firstElvesSections = (firstElves[0].toInt()..firstElves[1].toInt()).toList()
+            val secondElvesSections = (secondElves[0].toInt()..secondElves[1].toInt()).toList()
+
+            if (firstElvesSections.intersect(secondElvesSections).isNotEmpty()) {
+                result++
+            }
+
+        }
 
         return result
     }
 
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
-//    check(part2(testInput) == 70)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
